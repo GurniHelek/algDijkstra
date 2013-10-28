8a857640da27b26771316e6a7623f1e7120fcbff
Vlad Kovalenko <vo.kovalenko@gmail.com>
2013-10-28 22:58:08 +0200

    add graph container

--- algDijkstra/main.cpp
+++ algDijkstra/main.cpp
@@ -1,38 +1,96 @@
 #include <iostream>
-#include <vector>
-
-using namespace std;
+#include <set>
+#include <map>
 
 typedef struct edge_st
 {
-    int     dist;
-    struct edge_st* next;
-} edge_t;
+    int     vertex;
+    int     distance;
+    bool    isVisited;
+} edge;
+
+typedef std::map< int, std::set<edge> > graphContainer;
 
 class Graph
 {
-public:
-    vector<edge_t> graph;
+private:
+    int             numberOfVertices;
+    int             numberOfEdges;
+    graphContainer  graph;
 
+public:
     Graph();
-    Graph(int N);
+    Graph(int numberOfVertices);
+
+    int getNumberOfVertices();
+    int getNumberOfEdges();
+    graphContainer getGraph();
+    bool isAdjacent(int x, int y);
+    bool addEdge(int x, int y);
+    void makeRandomGraph(int density, int minDistance, int maxDistance);
 };
 
 Graph::Graph()
 {
-    graph[0].dist = 0;
-    graph[0].next = NULL;
+    numberOfVertices = 0;
+    numberOfEdges    = 0;
 }
 
-Graph::Graph(int N)
+Graph::Graph(int aNumberOfVertices)
 {
-    edge_t edge;
+    numberOfVertices = aNumberOfVertices;
 
-    for (int i = 0; i < N; i++)
-    {
-        edge.dist = 1;
-        edge.next = NULL;
-        graph.push_back(edge);
+    for (int i = 1; i <= numberOfVertices; i++) {
+        std::set<edge> edges;
+        graph.insert(std::pair< int, std::set<edge> >(i, edges));
+    }
+}
+
+int Graph::getNumberOfEdges()
+{
+    return numberOfEdges;
+}
+
+int Graph::getNumberOfVertices()
+{
+    return numberOfVertices;
+}
+
+graphContainer Graph::getGraph()
+{
+    return graph;
+}
+
+bool Graph::isAdjacent(int x, int y)
+{
+    if (x < 0 || x > numberOfVertices &&
+        y < 0 || y > numberOfVertices) {
+        return false;
+    }
+    graphContainer::iterator node = graph.find(x);
+
+}
+
+/**
+ * @brief Graph::addEdge
+ * @param x - first node to be connected
+ * @param y - second node to be connected
+ * @return true if this node was successfully connetced, false otherwise
+ */
+bool Graph::addEdge(int x, int y)
+{
+    if (x < 0 || x > numberOfVertices &&
+        y < 0 || y > numberOfVertices) {
+        return false;
+    }
+    graphContainer::iterator node = graph.find(x);
+
+}
+
+void Graph::makeRandomGraph(int density, int minDistance, int maxDistance)
+{
+    for (graphContainer::iterator iter = graph.begin(); iter != graph.end(); ++iter) {
+
     }
 }
 
