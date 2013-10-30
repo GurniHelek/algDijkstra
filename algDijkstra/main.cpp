aac9d826f86041991b2cddf192d19e7b9ae6c851
Vlad Kovalenko <vkovalenko@vk.(none)>
2013-10-30 19:12:43 +0200

    minor modifications

--- algDijkstra/main.cpp
+++ algDijkstra/main.cpp
@@ -2,6 +2,11 @@
 #include <map>
 #include <list>
 
+const int MIN_DENSITY   = 0;
+const int MAX_DENSITY   = 100;
+const int MIN_DISTANCE  = 1;
+const int MAX_DISTANCE  = 100;
+
 typedef struct edge_st
 {
     int     vertex;
@@ -26,8 +31,11 @@ public:
     int getNumberOfVertices();
     int getNumberOfEdges();
     graphContainer getGraph();
+    edgeContatiner::iterator getEdges(int x);
     bool isAdjacent(int x, int y);
     bool addEdge(int x, int y, int distance);
+    bool addVertex(int x);
+    bool deleteEdge(int x, int y);
     void makeRandomGraph(int density, int minDistance, int maxDistance);
     void showGraph();
 };
@@ -41,6 +49,7 @@ Graph::Graph()
 Graph::Graph(int aNumberOfVertices)
 {
     numberOfVertices = aNumberOfVertices;
+    numberOfEdges    = 0;
 
     for (int i = 1; i <= numberOfVertices; i++) {
         edgeContatiner edges;
@@ -65,7 +74,7 @@ graphContainer Graph::getGraph()
 
 bool Graph::isAdjacent(int x, int y)
 {
-    if ((x < 1 || x > numberOfVertices) &&
+    if ((x < 1 || x > numberOfVertices) ||
         (y < 1 || y > numberOfVertices)) {
         return false;
     }
@@ -82,6 +91,34 @@ bool Graph::isAdjacent(int x, int y)
     return false;
 }
 
+bool Graph::addVertex(int x)
+{
+    if (x <= numberOfVertices) {
+        return false;
+    }
+
+    edgeContatiner edges;
+    graph.insert(std::pair< int, edgeContatiner >(x, edges));
+    numberOfVertices++;
+
+    return true;
+}
+
+edgeContatiner::iterator Graph::getEdges(int x)
+{
+    edgeContatiner::iterator neighbors;
+
+    if (x < 1 || x > numberOfVertices) {
+        return neighbors;
+    }
+
+    graphContainer::iterator node = graph.find(x);
+
+    neighbors = node->second.begin();
+
+    return neighbors;
+}
+
 /**
  * @brief Graph::addEdge
  * @param x - first node to be connected
@@ -90,9 +127,20 @@ bool Graph::isAdjacent(int x, int y)
  */
 bool Graph::addEdge(int x, int y, int distance)
 {
-    if ((x < 1 || x > numberOfVertices) &&
-        (y < 1 || y > numberOfVertices)) {
+    if (x < 1 || y < 1) {
         return false;
+    }
+
+    if (x > numberOfVertices) {
+        for (int i = numberOfVertices; i <= x; ++i) {
+            addVertex(i);
+        }
+    }
+
+    if (y > numberOfVertices) {
+        for (int i = numberOfVertices; i <= y; ++i) {
+            addVertex(i);
+        }
     }
 
     if (isAdjacent(x, y)) {
@@ -106,6 +154,44 @@ bool Graph::addEdge(int x, int y, int distance)
     graphContainer::iterator nodeX = graph.find(y);
     edge newEdgeX = {x, distance, false};
     nodeX->second.push_back(newEdgeX);
+
+    numberOfEdges++;
+
+    return true;
+}
+
+bool Graph::deleteEdge(int x, int y)
+{
+    if ((x < 1 || x > numberOfVertices) ||
+        (y < 1 || y > numberOfVertices)) {
+        return false;
+    }
+
+    if (!isAdjacent(x, y)) {
+        return true;
+    }
+
+    graphContainer::iterator nodeX = graph.find(x);
+    for (edgeContatiner::iterator edgesX = nodeX->second.begin();
+         edgesX != nodeX->second.end();
+         edgesX++) {
+        if ((*edgesX).vertex == y) {
+            nodeX->second.erase(edgesX);
+            break;
+        }
+    }
+
+    graphContainer::iterator nodeY = graph.find(y);
+    for (edgeContatiner::iterator edgesY = nodeY->second.begin();
+         edgesY != nodeY->second.end();
+         edgesY++) {
+        if ((*edgesY).vertex == x) {
+            nodeY->second.erase(edgesY);
+            break;
+        }
+    }
+
+    numberOfEdges--;
 
     return true;
 }
@@ -123,20 +209,67 @@ void Graph::showGraph()
     }
 }
 
+//        output = min + (rand() % (int)(max - min + 1))
+
 void Graph::makeRandomGraph(int density, int minDistance, int maxDistance)
 {
+    if (density > MAX_DENSITY || density < MIN_DENSITY) {
+        density = MAX_DENSITY;
+    }
+
+    if (minDistance > maxDistance) {
+        int tmpDist = maxDistance;
+        maxDistance = minDistance;
+        minDistance = tmpDist;
+    }
+
+    if (minDistance > MAX_DISTANCE || minDistance < MIN_DISTANCE) {
+        minDistance = MAX_DISTANCE;
+    }
+
+    if (maxDistance > MAX_DISTANCE || maxDistance < MIN_DISTANCE) {
+        maxDistance = MAX_DISTANCE;
+    }
+
     for (graphContainer::iterator iter = graph.begin(); iter != graph.end(); ++iter) {
+
 
     }
 }
 
 int main()
 {
-    Graph g = Graph(3);
+/*
+    Graph g = Graph(4);
+
+
     g.addEdge(1, 2, 13);
     g.addEdge(2, 3, 6);
+    g.addEdge(5, 3, 6);
+    g.addEdge(1, 4, 6);
+    g.addEdge(1, 3, 6);
+    std::cout << "Number of v : " << g.getNumberOfVertices() << std::endl;
+    std::cout << "Number of e : " << g.getNumberOfEdges() << std::endl;
+
+//    g.makeRandomGraph();
 
     g.showGraph();
+
+    g.deleteEdge(1, 2);
+
+    std::cout << "Number of v : " << g.getNumberOfVertices() << std::endl;
+    std::cout << "Number of e : " << g.getNumberOfEdges() << std::endl;
+    g.showGraph();
+*/
+    Graph rg = Graph();
+
+    rg.makeRandomGraph(10, 1, 5);
+
+    std::cout << "Number of v : " << rg.getNumberOfVertices() << std::endl;
+    std::cout << "Number of e : " << rg.getNumberOfEdges() << std::endl;
+    rg.showGraph();
+
+    std::cout << "END \n";
 
     return 0;
 }
